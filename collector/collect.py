4ee4f8b85a70f6717430a9cef68c50f7d23b35c8
JasonWang <1103870790@qq.com>
2020-03-10 00:24:14 +0800

    add selenium

--- collector/collect.py
+++ collector/collect.py
@@ -1,7 +1,27 @@
 import uuid
 import models
+import requests
+from selenium import webdriver
+import time
+
+
+def stock_list():
+    main_url = 'http://vip.stock.finance.sina.com.cn/mkt/'
+
+    driver = webdriver.Chrome()
+    driver.get(main_url)
+    driver.implicitly_wait(10)
+    lv1 = driver.find_element_by_xpath('//a[text()="申万行业"]')
+    lv1.click()
+
+    with open('out.html', 'w', encoding='gb2312') as f:
+        f.write(driver.page_source)
+
+    driver.quit()
 
 
 if __name__ == "__main__":
-    models.db.connect()
-    models.db.create_tables([models.Exchange, models.Market, models.Currency])
+    # models.db.connect()
+    # models.db.create_tables([models.Exchange, models.Market, models.Currency])
+    stock_list()
+    
