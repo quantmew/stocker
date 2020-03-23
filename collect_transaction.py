a9372ce37d0637628d75c6a1e5ff8a9edaf1ab8d
JasonWang <1103870790@qq.com>
2020-03-23 23:21:24 +0800

    better transaction collector

--- collect_transaction.py
+++ collect_transaction.py
@@ -8,16 +8,30 @@ import uuid
 import time
 import random
 import tqdm
+import logging
 from multiprocessing import Queue
 from decimal import *
 from models import *
+import h5py
+import csv
+import os
 
 周六 = 6
 周日 = 7
+csv_dir = "./transaction/"
 obj_list = Queue()
 
 proxies = { "http": "http://127.0.0.1:10805", "https": "http://127.0.0.1:10805"}
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
+
+# logging
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+handler = logging.FileHandler('transaction_collect.log', mode='a', encoding='utf-8')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 
 def 取第一个元素(数组):
     返回值 = None
@@ -48,138 +62,113 @@ def get_stock_number(stock):
     新浪链接股票编号 = 交易所缩写.lower() + 六位编号
     return 新浪链接股票编号
 
+async def get_a_stock_transaction(stock):
+    新浪链接股票编号 = get_stock_number(stock)
 
-async def get_stock_history_date(obj_list, req):
-    股票 = req[0]
-    当前日期 = req[1]
-    页码 = req[2]
-    新浪链接股票编号 = get_stock_number(股票)
-    链接 = f'http://market.finance.sina.com.cn/transHis.php?symbol={新浪链接股票编号}&date={当前日期.strftime("%Y-%m-%d")}&page={str(页码)}'
+    开始日期 = stock.list_date
+    结束日期 = datetime.date.today()
+
+    if 开始日期 < datetime.date(2005, 1, 1):
+        开始日期 = datetime.date(2005, 1, 1)
     
-    页面 = None
-    try:
-        页面 = await async_get(链接)
-    except Exception as e:
-        print('requests.exceptions.ConnectionError' + str(e))
-        obj_list.put(req)
-        return
+    当前日期 = 开始日期
+    一天 = datetime.timedelta(days=1)
+    while 当前日期 <= 结束日期:
+        if 当前日期.isoweekday() in [周六, 周日]:
+            当前日期 += 一天
+            continue
+        csv_filename = csv_dir+f'{新浪链接股票编号}+{当前日期.strftime("%y-%m-%d")}.csv'
+        if os.path.exists(csv_filename):
+            当前日期 += 一天
+            continue
+        data = []
 
-    if 页面 is None:
-        obj_list.put(req)
-        return
-    print(链接)
-    页面.encoding='gb2312'
-    网页语法树 = etree.HTML(页面.text)
+        页码 = 1
+        while True:
+            链接 = f'http://market.finance.sina.com.cn/transHis.php?symbol={新浪链接股票编号}&date={当前日期.strftime("%Y-%m-%d")}&page={str(页码)}'
+            页面 = None
+            try:
+                页面 = await async_get(链接)
+            except Exception as e:
+                logger.error('requests.exceptions.ConnectionError' + str(e))
+                break
+            if 页面 is None:
+                break
+                    
+            logger.info(链接)
+            页面.encoding='gb2312'
+            网页语法树 = etree.HTML(页面.text)
 
-    if 网页语法树 is None:
-        obj_list.put(req)
-        print('语法树解析失败')
+            if 网页语法树 is None:
+                logger.error('语法树解析失败')
 
-        # 判断封禁
-        页面.encoding='utf-8'
-        网页语法树 = etree.HTML(页面.text)
+                # 判断封禁
+                页面.encoding='utf-8'
+                if '拒绝访问' in 页面.text:
+                    logger.fatal('ip已被新浪封禁')
+                break
 
-        if 网页语法树 is None:
-            return
+            # 真的没有数据了
+            错误信息 = 取第一个元素(网页语法树.xpath('//div[contains(text(),"输入的代码有误或没有交易数据")]'))
+            
+            if 错误信息 is not None:
+                break
+            
+            # 没有数据了
+            数据 = 网页语法树.xpath('//table[@id="datatbl"]/tbody/tr')
+            if len(数据) == 0:
+                break
 
-        拒绝访问 = 取第一个元素(网页语法树.xpath('//h1[text()="拒绝访问"]'))
+            for 每一行数据 in 数据:
+                成交时间 = 取第一个元素(每一行数据.xpath('./th[1]/text()'))
+                成交价 = 取第一个元素(每一行数据.xpath('./td[1]/text()'))
+                价格变动 = 取第一个元素(每一行数据.xpath('./td[2]/text()'))
+                成交量 = 取第一个元素(每一行数据.xpath('./td[3]/text()'))
+                成交额 = 取第一个元素(每一行数据.xpath('./td[4]/text()'))
+                性质 = 取第一个元素(每一行数据.xpath('./th[2]/*[name()="h6" or name()="h5" or name()="h1"]/text()'))
 
-        if 拒绝访问 is not None:
-            print('ip已被新浪封禁')
+                时, 分, 秒 = 成交时间.split(':')
+                成交价 = Decimal(成交价)
+                if 价格变动 is None:
+                    价格变动 = Decimal(0)
+                elif '--' in 价格变动:
+                    价格变动 = Decimal(0)
+                else:
+                    价格变动 = Decimal(价格变动)
+                成交量 = int(成交量)
+                if 成交额 is None:
+                    成交额 = Decimal(0)
+                else:
+                    成交额 = Decimal(成交额.replace(',', ''))
 
-        return
+                data.append((
+                    新浪链接股票编号,
+                    datetime.datetime(
+                        当前日期.year,
+                        当前日期.month,
+                        当前日期.day,
+                        int(时), int(分), int(秒)),
+                    成交价,
+                    价格变动,
+                    成交量,
+                    成交额,
+                    性质,
+                ))
 
-    错误信息 = 取第一个元素(网页语法树.xpath('//div[contains(text(),"输入的代码有误或没有交易数据")]'))
-    
-    if 错误信息 is not None:
-        return
+            页码 += 1
 
-    数据 = 网页语法树.xpath('//table[@id="datatbl"]/tbody/tr')
-    if len(数据) == 0:
-        return
+        with open(csv_filename,'w', newline='', encoding='utf-8') as f:
+            csv_write = csv.writer(f)
+            csv_head = ["stock_symbol","datetime", "price", "price_change", "volume", "turnover", "kind"]
+            csv_write.writerow(csv_head)
+            csv_write.writerows(data)
 
-    for 每一行数据 in 数据:
-        成交时间 = 取第一个元素(每一行数据.xpath('./th[1]/text()'))
-        成交价 = 取第一个元素(每一行数据.xpath('./td[1]/text()'))
-        价格变动 = 取第一个元素(每一行数据.xpath('./td[2]/text()'))
-        成交量 = 取第一个元素(每一行数据.xpath('./td[3]/text()'))
-        成交额 = 取第一个元素(每一行数据.xpath('./td[4]/text()'))
-        性质 = 取第一个元素(每一行数据.xpath('./th[2]/*[name()="h6" or name()="h5" or name()="h1"]/text()'))
-
-        时, 分, 秒 = 成交时间.split(':')
-        成交价 = Decimal(成交价)
-        if 价格变动 is None:
-            价格变动 = Decimal(0)
-        elif '--' in 价格变动:
-            价格变动 = Decimal(0)
-        else:
-            价格变动 = Decimal(价格变动)
-        成交量 = int(成交量)
-        if 成交额 is None:
-            成交额 = Decimal(0)
-        else:
-            成交额 = Decimal(成交额.replace(',', ''))
-
-        交易对象, 是否创建 = Transaction.get_or_create(
-            equity = 股票,
-            time = datetime.datetime(
-                当前日期.year,
-                当前日期.month,
-                当前日期.day,
-                int(时), int(分), int(秒)),
-            defaults = {
-                'uuid': uuid.uuid4(),
-                'price': 成交价,
-                'price_change': 价格变动,
-                'volume': 成交量,
-                'turnover': 成交额,
-                'kind': 性质,
-            }
-        )
-
-    obj_list.put((股票, 当前日期, 页码 + 1))
+        当前日期 += 一天
 
 
 async def main():
-    tmp_list = []
     for 股票 in tqdm.tqdm(iterable=Equities.select(), ascii=True):
-        开始日期 = 股票.list_date
-        结束日期 = datetime.date.today()
-
-        if 开始日期 < datetime.date(2005, 1, 1):
-            开始日期 = datetime.date(2005, 1, 1)
-        
-        当前日期 = 开始日期
-        一天 = datetime.timedelta(days=1)
-        while 当前日期 <= 结束日期:
-            
-            if 当前日期.isoweekday() in [周六, 周日]:
-                当前日期 += 一天
-                continue
-            页码 = 1
-            tmp_list.append((股票, 当前日期, 页码))
-            当前日期 += 一天
-
-    random.shuffle(tmp_list)
-    for each_obj in tmp_list:
-        obj_list.put(each_obj)
-
-    while True:
-        # await asyncio.sleep(0.05 * random.random())
-        await asyncio.sleep(0)
-
-        # if obj_list.empty():
-        #    break
-        if obj_list.qsize() % 100 == 0:
-            print(f'queue length: {obj_list.qsize()}')
-           
-        req = obj_list.get()
-        if req is None:
-            await asyncio.sleep(1)
-            print('no new requests')
-            continue
-        
-        asyncio.create_task(get_stock_history_date(obj_list, req))
+        await get_a_stock_transaction(股票)
 
 if __name__ == "__main__":
     asyncio.run(main())
